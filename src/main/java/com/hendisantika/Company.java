0010009fffa1d2514d9f629abe4fc2f20536e470
Hendi Santika <hendisantika@gmail.com>
2019-12-08 08:08:45 +0700

    #17 - Upgraded into Spring Boot 2.2.0.RELEASE version

--- src/main/java/com/hendisantika/Company.java
+++ src/main/java/com/hendisantika/Company.java
@@ -1,26 +1,27 @@
 package com.hendisantika;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-@XmlRootElement(name="company-info", namespace="com.hendisantika" )
+
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement(name = "company-info", namespace = "com.hendisantika")
 @XmlAccessorType(XmlAccessType.NONE)
 public class Company {
-	@XmlAttribute(name="id")
+	@XmlAttribute(name = "id")
 	private Integer id;
-	@XmlElement(name="company-name")
+	@XmlElement(name = "company-name")
 	private String companyName;
-	@XmlElement(name="ceo-name")
+	@XmlElement(name = "ceo-name")
 	private String ceoName;
-	@XmlElement(name="no-emp")
+	@XmlElement(name = "no-emp")
 	private Integer noEmp;
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getCompanyName() {
 		return companyName;
 	}
